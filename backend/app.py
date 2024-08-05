917eeddb8fa22e84ef4aefb0502bf101b7b3ebaf
Shivek Sharma <shiveksharma200225@gmail.com>
2024-08-05 17:34:58 +0530

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -45,4 +45,6 @@ def get_flight_status():
     return jsonify(flights), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
